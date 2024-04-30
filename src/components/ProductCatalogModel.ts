9845bbdceb1769a1697eb6aff45a9b64ae754eb9
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-04-30 20:14:35 +0300

    Описан класс ProductCatalogModel

---
+++ src/components/ProductCatalogModel.ts
@@ -0,0 +1,20 @@
+import { IProductCatalogModel, IProductModel } from "../types";
+
+class ProductCatalogModel implements IProductCatalogModel {
+
+    items: IProductModel[];
+
+    constructor(items: IProductModel[]) {
+        this.setItems(items);
+    }
+
+    setItems(items: IProductModel[]): void {
+        this.items = items;
+    }
+
+    getProductById(id: string): IProductModel | null {
+        const idArr = this.items.map(item => item.id);
+        const searchIndex = idArr.indexOf(id);
+        return searchIndex === -1 ? null : this.items[searchIndex];
+    }
+}
