392c70252f868d036ce0f60892fed470ba83d400
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-01 19:26:38 +0300

    Описан класс CatalogItemView

--- src/components/CatalogItemView.ts
+++ src/components/CatalogItemView.ts
@@ -2,7 +2,29 @@ import { IEventEmitter, IView } from "../types";
 
 export class CatalogItemView implements IView {
 
-    constructor (protected container: HTMLElement, protected events: IEventEmitter) {
+    protected category: HTMLSpanElement;
+    protected title: HTMLHeadingElement;
+    protected img: HTMLImageElement;
+    protected price: HTMLSpanElement;
 
+    protected id: string | null = null;
+
+    constructor(protected container: HTMLButtonElement, protected events: IEventEmitter) {
+        this.category = this.container.querySelector('.card__category') as HTMLSpanElement;
+        this.title = this.container.querySelector('.card__title') as HTMLHeadingElement;
+        this.img = this.container.querySelector('.card__image') as HTMLImageElement;
+        this.price = this.container.querySelector('.card__price') as HTMLSpanElement;
+
+        this.container.addEventListener('click', () => {
+            this.events.emit('ui:catalog-item-open', {id: this.id})
+        })
+    }
+
+    render(data: {id: string}): HTMLElement {
+        if (data) {
+            this.id = data.id;
+        }
+
+        return this.container;
     }
 }
