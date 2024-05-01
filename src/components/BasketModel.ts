46a6754b371b5aa8b328078b065224f050072967
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-01 11:06:50 +0300

    Добавлены обработки IEventEmitter

--- src/components/BasketModel.ts
+++ src/components/BasketModel.ts
@@ -1,23 +1,26 @@
-import { IBasketModel, TBasketItem, TBasketItems } from "../types";
+import { IBasketModel, IEventEmitter, TBasketItem, TBasketItems } from "../types";
 
 export class BasketModel implements IBasketModel {
     items: TBasketItems;
     totalCost: number;
 
-    constructor() {
+    constructor(protected events: IEventEmitter) {
         this.items = {};
     }
 
     addProduct(id: string, item: TBasketItem): void {
         this.items[id] = item;
+        this._changed();
     }
 
     removeProduct(id: string): void {
         delete this.items[id];
+        this._changed();
     }
 
     clear(): void {
         this.items = {};
+        this._changed();
     }
 
     getItems(): TBasketItems {
@@ -31,5 +34,9 @@ export class BasketModel implements IBasketModel {
         }
 
         return cost;
+    }
+
+    protected _changed() {
+        this.events.emit('basket:change', this.getItems());
     }
 }
