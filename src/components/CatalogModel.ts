8c6ef73787d273b2b2ec80fad26d026514036fb1
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-01 21:33:06 +0300

    Добавлены обработки EventEmitter

--- src/components/CatalogModel.ts
+++ src/components/CatalogModel.ts
@@ -1,21 +1,26 @@
-import { ICatalogModel, IProductModel } from "../types";
+import { ICatalogModel, IEventEmitter, IProductModel } from "../types";
 
 export class CatalogModel implements ICatalogModel {
 
     items: IProductModel[];
 
-    constructor(items?: IProductModel[]) {
+    constructor(protected events: IEventEmitter, items?: IProductModel[]) {
         if (items) this.setItems(items);
         else this.items = [];
     }
 
     setItems(items: IProductModel[]): void {
         this.items = items;
+        this._changed();
     }
     
     getProductById(id: string): IProductModel | null {
         const idArr = this.items.map(item => item.id);
         const searchIndex = idArr.indexOf(id);
         return searchIndex === -1 ? null : this.items[searchIndex];
+    }
+
+    protected _changed() {
+        this.events.emit('catalog:change', {items: this.items});
     }
 }
