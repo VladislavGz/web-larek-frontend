3ddf1607d437dd67d0e1a241796f19d09d7235f3
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-04 20:33:45 +0300

    Описан класс SuccessView

---
+++ src/components/SuccessView.ts
@@ -0,0 +1,24 @@
+import { IEventEmitter, IView } from "../types";
+
+export class SuccessView implements IView {
+
+    protected description: HTMLParagraphElement;
+    protected successBtn: HTMLButtonElement;
+
+    constructor(protected container: HTMLElement, protected events: IEventEmitter) {
+        this.description = this.container.querySelector('.order-success__description') as HTMLParagraphElement;
+        this.successBtn = this.container.querySelector('.order-success__close') as HTMLButtonElement;
+
+        this.successBtn.addEventListener('click', () => {
+            this.events.emit('ui:successButton', {});
+        });
+    }
+
+    render(data: {total: number}): HTMLElement {
+        if (data) {
+            this.description.textContent = `Списано ${data.total} синапсов`;
+        }
+
+        return this.container;
+    }
+}
