0f53b5f3f8872257f274b87eaee3aaa5adb39f5f
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-04 21:09:46 +0300

    Обработчики событий вынесены в отдельные колбэки

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import { API_URL, CDN_URL } from './utils/constants';
 import { cloneTemplate } from './utils/utils';
 
 /*------------------------------------------------------------------------------------------------------------------------------------*/
+/*ИНИЦИАЛИЗАЦИЯ*/
 //шаблоны
 const catalogItemTemplate = document.querySelector('#card-catalog') as HTMLTemplateElement;
 const cardFullTemplate = document.querySelector('#card-preview') as HTMLTemplateElement;
@@ -46,51 +47,42 @@ const contactsForm = new FormContactsView(cloneTemplate(contactsTemplate), event
 const success = new SuccessView(cloneTemplate(successTemplate), events);
 
 /*------------------------------------------------------------------------------------------------------------------------------------*/
-//событие изменения модели каталога товаров
-events.on('catalog:change', (data: { items: IProductModel[] }) => {
-    console.log('EVT: catalog:change')
-
+/*ОБРАБОТЧИКИ СОБЫТИЙ*/
+//обработка изменения модели каталога товаров
+function handleCatalogChange(data: { items: IProductModel[] }) {
     let itemsContainers = data.items.map(item => {
         return new CardCatalogView(cloneTemplate(catalogItemTemplate), events).render({ product: item, cdn: CDN_URL });
     })
 
     catalog.render({ items: itemsContainers });
-});
+}
 
-//событие клика на карточку товара в каталоге
-events.on('ui:catalog-item-open', (data: { id: string }) => {
-    console.log(`EVT: ui:catalog-item-open | id: ${data.id}`);
-
+//обработка клика на карточку товара в каталоге
+function handleCatalogItemOpen(data: { id: string }) {
     const cardContainer = cardFull.render({ product: catalogModel.getProductById(data.id), cdn: CDN_URL, inBasket: basketModel.checkProductById(data.id) });
     modal.render(cardContainer)
     modal.open();
-})
+}
 
-//событие закрытия модального окна
-events.on('ui:modal-closeModal', () => {
-    console.log('EVT: ui:modal-closeModal');
-
+//обработка закрытия модального окна
+function handleCloseModal() {
     modal.close();
-});
+}
 
-//событие клика по кнопке добавления товара в корзину
-events.on('ui:cardFull-addBasket', (data: { id: string }) => {
-    console.log(`EVT: ui:cardFull-addBasket | id: ${data.id}`);
-
+//обработка клика по кнопке добавления товара в корзину
+function handleAddBasketButton(data: { id: string }) {
     if (basketModel.checkProductById(data.id)) {
         basketModel.removeProduct(data.id);
     } else {
         const product: TBasketItem = catalogModel.getProductById(data.id);
         basketModel.addProduct(product);
     }
-    
+
     modal.close();
-});
+}
 
-//событие изменения содержимого корзины
-events.on('basket:change', (data: TBasketItems) => {
-    console.log(`EVT: basket:change | count: ${basketModel.count}`);
-
+//обработка изменения содержимого корзины
+function handleBasketChange(data: TBasketItems) {
     basketIcon.render({ count: basketModel.count });
 
     const itemsContainers = Object.values(data).map((item, i) => {
@@ -98,87 +90,121 @@ events.on('basket:change', (data: TBasketItems) => {
     })
 
     basket.render({ items: itemsContainers, totalCost: basketModel.totalCost });
-});
+}
 
-//событие клика по иконке корзины
-events.on('ui:basketIcon-openBasket', () => {
-    console.log(`EVT: ui:basketIcon-openBasket`);
-
+//обработка клика по иконке корзины
+function handleOpenBasket() {
     const basketContainer = basket.render(null);
     modal.render(basketContainer);
     modal.open();
-});
+}
 
-//событие клика по кнопке удаления товара из корзины
-events.on('ui:basket-remove-item', (data: {id: string}) => {
-    console.log(`EVT: ui:basket-remove-item | id: ${data.id}`);
-
+//обработка клика по кнопке удаления товара из корзины
+function handleBasketRemoveItem(data: { id: string }) {
     basketModel.removeProduct(data.id);
-});
+}
 
-//событие клика по кнопке оформления заказа
-events.on('ui:basket-order', () => {
-    console.log(`EVT: ui:basket-order`);
-
+//обработка клика по кнопке оформления заказа
+function handleBasketOrderButton() {
     modal.render(orderForm.render(userModel.getUserData()));
-});
+}
 
-//событие клика по кнопке выбора способа оплаты
-events.on('ui:orderForm-paymentMethodButton', (data: {paymentMethod: TPaymentMethod}) => {
-    console.log(`EVT: ui:orderForm-paymentMethodButton | method: ${data.paymentMethod}`);
+//обработка клика по кнопке выбора способа оплаты
+function handlePaymentButton(data: { paymentMethod: TPaymentMethod }) {
     userModel.paymentMethod = data.paymentMethod;
-});
+}
 
-//событие инпута поле ввода адреса доставки
-events.on('ui:orderForm-addressInput', (data: {address: string}) => {
-    console.log(`EVT: ui:orderForm-addressInput | address: ${data.address}`);
+//обработка инпута поле ввода адреса доставки
+function handleAdressInput(data: { address: string }) {
     userModel.address = data.address;
-});
+}
 
-//событие сабмита формы заказа
-events.on('ui:orderForm-submit', () => {
-    console.log(`EVT: ui:orderForm-submit`);
-
+//обработка сабмита формы заказа
+function handleOrderFormSubmit() {
     modal.render(contactsForm.render(userModel.getUserData()));
-});
+}
 
-//событие инпута поле ввода email
-events.on('ui:contactsForm-emailInput', (data: {email: string}) => {
-    console.log(`EVT: ui:contactsForm-emailInput | email: ${data.email}`);
+//обработка инпута поле ввода email
+function handleEmailInput(data: { email: string }) {
     userModel.email = data.email;
-});
+}
 
-//событие инпута поле ввода телефона
-events.on('ui:contactsForm-phoneInput', (data: {phone: string}) => {
-    console.log(`EVT: ui:contactsForm-phoneInput | phone: ${data.phone}`);
+//обработка инпута поле ввода телефона
+function handlePhoneInput(data: { phone: string }) {
     userModel.phone = data.phone;
-});
+}
 
-//событие сабмита формы контактов
-events.on('ui:contactsForm-submit', () => {
-    console.log(`EVT: ui:contactsForm-submit`);
-
+//обработка сабмита формы контактов
+function hancleContactsFormSubmit() {
     api.post('/order', {
         ...userModel.getUserData(),
         ...basketModel.getBasketOrderData()
     }).then((result: OrderData) => {
-        modal.render(success.render({total: result.total}))
+        modal.render(success.render({ total: result.total }))
     })
-});
+}
 
-//событие изменения данных пользователя
-events.on('user:change', (userData: TUserData) => {
-    console.log(`EVT: user:change`);
-
+//обработка изменения данных пользователя
+function handleUserDataChange(userData: TUserData) {
     orderForm.render(userData);
     contactsForm.render(userData);
-});
+}
 
-//событие клика по кнопке успешного завершения заказа
-events.on('ui:successButton', () => {
+//обработка клика по кнопке успешного завершения заказа
+function handleSuccessButton() {
     basketModel.clear();
     modal.close();
-})
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+/*АКТИВАЦИЯ СОБЫТИЙ*/
+//событие изменения модели каталога товаров
+events.on('catalog:change', handleCatalogChange);
+
+//событие клика на карточку товара в каталоге
+events.on('ui:catalog-item-open', handleCatalogItemOpen)
+
+//событие закрытия модального окна
+events.on('ui:modal-closeModal', handleCloseModal);
+
+//событие клика по кнопке добавления товара в корзину
+events.on('ui:cardFull-addBasket', handleAddBasketButton);
+
+//событие изменения содержимого корзины
+events.on('basket:change', handleBasketChange);
+
+//событие клика по иконке корзины
+events.on('ui:basketIcon-openBasket', handleOpenBasket);
+
+//событие клика по кнопке удаления товара из корзины
+events.on('ui:basket-remove-item', handleBasketRemoveItem);
+
+//событие клика по кнопке оформления заказа
+events.on('ui:basket-order', handleBasketOrderButton);
+
+//событие клика по кнопке выбора способа оплаты
+events.on('ui:orderForm-paymentMethodButton', handlePaymentButton);
+
+//событие инпута поле ввода адреса доставки
+events.on('ui:orderForm-addressInput', handleAdressInput);
+
+//событие сабмита формы заказа
+events.on('ui:orderForm-submit', handleOrderFormSubmit);
+
+//событие инпута поле ввода email
+events.on('ui:contactsForm-emailInput', handleEmailInput);
+
+//событие инпута поле ввода телефона
+events.on('ui:contactsForm-phoneInput', handlePhoneInput);
+
+//событие сабмита формы контактов
+events.on('ui:contactsForm-submit', hancleContactsFormSubmit);
+
+//событие изменения данных пользователя
+events.on('user:change', handleUserDataChange);
+
+//событие клика по кнопке успешного завершения заказа
+events.on('ui:successButton', handleSuccessButton);
 
 /*------------------------------------------------------------------------------------------------------------------------------------*/
 //запрашиваем данные всех товаров с сервера
