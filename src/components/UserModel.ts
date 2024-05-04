ccb7c1c5fccd301c4cf3bccc3266b17f96bf3401
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-04 13:56:36 +0300

    Добавлены обработки события изменения данных пользователя

--- src/components/UserModel.ts
+++ src/components/UserModel.ts
@@ -1,4 +1,4 @@
-import { IEventEmitter, IUserModel, TPaymentMethod } from "../types";
+import { IEventEmitter, IUserModel, TPaymentMethod, TUserData } from "../types";
 
 export class UserModel implements IUserModel {
     private _paymentMethod: TPaymentMethod;
@@ -17,35 +17,35 @@ export class UserModel implements IUserModel {
         return this._paymentMethod;
     }
     set paymentMethod(method: TPaymentMethod) {
-        this.events.emit('user:change', this.getUserData());
         this._paymentMethod = method;
+        this.events.emit('user:change', this.getUserData());
     }
 
     get address(): string {
         return this._address;
     }
     set address(address: string) {
-        this.events.emit('user:change', this.getUserData());
         this._address = address;
+        this.events.emit('user:change', this.getUserData());
     }
 
     get email(): string {
         return this._email;
     }
     set email(email: string) {
-        this.events.emit('user:change', this.getUserData());
         this._email = email;
+        this.events.emit('user:change', this.getUserData());
     }
 
     get phone(): string {
         return this._phone;
     }
     set phone(phone: string) {
-        this.events.emit('user:change', this.getUserData());
         this._phone = phone;
+        this.events.emit('user:change', this.getUserData());
     }
 
-    getUserData = () => ({
+    getUserData = (): TUserData => ({
         paymentMethod: this._paymentMethod,
         address: this._address,
         email: this._email,
