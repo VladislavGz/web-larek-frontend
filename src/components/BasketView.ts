cc6e2a4cdb36cb47d40fc1b392e49851e9d4cfe6
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-05-04 19:06:28 +0300

    Добавлена проверка на пустую корзину для активации(деактивации) кнопки оформления заказа

--- src/components/BasketView.ts
+++ src/components/BasketView.ts
@@ -1,3 +1,4 @@
+import { electron } from "webpack";
 import { IEventEmitter, IView } from "../types";
 
 export class BasketView implements IView {
@@ -14,12 +15,25 @@ export class BasketView implements IView {
         this.orderBtn.addEventListener('click', () => {
             this.events.emit('ui:basket-order', {});
         });
+
+        this.disableOrderButton();
+    }
+
+    enableOrderButton(): void {
+        this.orderBtn.removeAttribute('disabled');
+    }
+
+    disableOrderButton(): void {
+        this.orderBtn.setAttribute('disabled', '');
     }
 
     render(data: { items: HTMLElement[], totalCost: number }): HTMLElement {
         if (data) {
             this.basketList.replaceChildren(...data.items);
             this.totalPrice.textContent = `${data.totalCost} синапсов`;
+
+            if (data.items.length > 0) { this.enableOrderButton() }
+            else { this.disableOrderButton() }
         }
 
         return this.container;
